0068d52196205a56ab678603e02bb4746a6c103b
Nitish Kumar <thisisnitish30@gmail.com>
2024-04-28 19:33:38 +0530

    few fixes

---
+++ go.mod
@@ -0,0 +1,3 @@
+module meru
+
+go 1.22.2
---
+++ src/token/token.go
@@ -0,0 +1,34 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL" // illegal token that don't know
+	EOF     = "EOF"     // end of the file
+
+	// Identifiers + literals
+	IDENTFIER = "IDENTFIER" // add, foobar, x, y, z...
+	INT       = "INT"       // 1,2,3,4,5...
+
+	// Operators
+	ASSIGNMENT = "="
+	PLUS       = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LEFTPARENTHESIS  = "("
+	RIGHTPARENTHESIS = ")"
+	LEFTBRACKET      = "{"
+	RIGHTBRACKET     = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
