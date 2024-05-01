0672ca473b9ff81c27f503f0809bcfea11782132
Nitish Kumar <thisisnitish30@gmail.com>
2024-05-01 12:58:08 +0530

    next token method done

--- src/lexer/lexer.go
+++ src/lexer/lexer.go
@@ -1,5 +1,7 @@
 package lexer
 
+import "meru/src/token"
+
 type Lexer struct {
 	input        string
 	position     int  // current position in input (points to current char)
@@ -28,4 +30,38 @@ func (l *Lexer) readChar() {
 
 	l.position = l.readPosition
 	l.readPosition += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGNMENT, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LEFTPARENTHESIS, l.ch)
+	case ')':
+		tok = newToken(token.RIGHTPARENTHESIS, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LEFTBRACKET, l.ch)
+	case '}':
+		tok = newToken(token.RIGHTBRACKET, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+
+	}
+
+	l.readChar()
+	return tok
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
