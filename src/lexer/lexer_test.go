4d799bc4e6244f2cd828f97e71122d1267361179
Nitish Kumar <thisisnitish30@gmail.com>
2024-05-01 13:17:36 +0530

    correct spelling mistake

--- src/lexer/lexer_test.go
+++ src/lexer/lexer_test.go
@@ -6,20 +6,79 @@ import (
 	"meru/src/token"
 )
 
+// TODO: Add all test cases at last
 func TestNextToken(t *testing.T) {
-	input := `=+(){},;`
+
+	/*
+		  // Test Case 1
+		  input := `=+(){},;`
+
+			tests := []struct {
+				expectedType    token.TokenType
+				expectedLiteral string
+			}{
+				{token.ASSIGNMENT, "="},
+				{token.PLUS, "+"},
+				{token.LEFTPARENTHESIS, "("},
+				{token.RIGHTPARENTHESIS, ")"},
+				{token.LEFTBRACKET, "{"},
+				{token.RIGHTBRACKET, "}"},
+				{token.COMMA, ","},
+				{token.SEMICOLON, ";"},
+				{token.EOF, ""},
+			}
+	*/
+
+	input := `
+  let five = 5;
+  let ten = 10;
+
+  let add = fn(x, y){
+    x+y;
+  };
+
+  let result = add(five, ten);
+  `
 
 	tests := []struct {
 		expectedType    token.TokenType
 		expectedLiteral string
 	}{
+		{token.LET, "let"},
+		{token.IDENTIFIER, "five"},
 		{token.ASSIGNMENT, "="},
-		{token.PLUS, "+"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENTIFIER, "ten"},
+		{token.ASSIGNMENT, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENTIFIER, "add"},
+		{token.ASSIGNMENT, "="},
+		{token.FUNCTION, "fn"},
 		{token.LEFTPARENTHESIS, "("},
+		{token.IDENTIFIER, "x"},
+		{token.COMMA, ","},
+		{token.IDENTIFIER, "y"},
 		{token.RIGHTPARENTHESIS, ")"},
 		{token.LEFTBRACKET, "{"},
+		{token.IDENTIFIER, "x"},
+		{token.PLUS, "+"},
+		{token.IDENTIFIER, "y"},
+		{token.SEMICOLON, ";"},
 		{token.RIGHTBRACKET, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENTIFIER, "result"},
+		{token.ASSIGNMENT, "="},
+		{token.IDENTIFIER, "add"},
+		{token.LEFTPARENTHESIS, "("},
+		{token.IDENTIFIER, "five"},
 		{token.COMMA, ","},
+		{token.IDENTIFIER, "ten"},
+		{token.RIGHTPARENTHESIS, ")"},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
