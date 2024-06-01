01cc924b9d37c4638c36c7913c8e0ff10aaa2dbe
Nitish Kumar <thisisnitish30@gmail.com>
2024-06-01 13:04:25 +0530

    ast layout done

---
+++ src/ast/ast.go
@@ -0,0 +1,57 @@
+package ast
+
+import "meru/src/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+// A Program is a series of statements
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+// let statements have 3 things
+type LetStatement struct {
+	Token token.Token // the token.LET token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+type Identifier struct {
+	Token token.Token // the token.IDENTIFIER token
+	Value string
+}
+
+// Reason for adding statement and token to identifier is that we could have a value producing identifier
+// It is there to keep things simple
+
+func (id *Identifier) statementNode() {}
+
+func (id *Identifier) TokenLiteral() string {
+	return id.Token.Literal
+}
