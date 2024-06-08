9568be2f7dee07fb6056bd4595db84a9a2c63033
Nitish Kumar <thisisnitish30@gmail.com>
2024-06-08 12:24:44 +0530

    parser layout done

---
+++ src/parser/parser.go
@@ -0,0 +1,32 @@
+package parser
+
+import (
+	"meru/src/ast"
+	"meru/src/lexer"
+	"meru/src/token"
+)
+
+type Parser struct {
+	l         *lexer.Lexer
+	currToken token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	// Read two tokens, so currToken and peekToken are both set
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.currToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
