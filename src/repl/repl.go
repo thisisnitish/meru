76340bb44947446dbae8bd1685f4e741ae3fc72d
Nitish Kumar <thisisnitish30@gmail.com>
2024-10-05 12:12:20 +0530

    injected parser in repl

--- src/repl/repl.go
+++ src/repl/repl.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"io"
 	"meru/src/lexer"
-	"meru/src/token"
+	"meru/src/parser"
 )
 
 const PROMPT = ">> "
@@ -23,9 +23,26 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+
+		// for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+		// 	fmt.Printf("%+v\n", tok)
+		// }
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
